8bc324654ea848512740632adbb0fe1cdce34a18
bhuvitw <bhuvnesh6115@gmail.com>
2026-07-19 11:13:51 +0530

    reverseString.c

---
+++ C/reverseString.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Data Structure";
+    int left = 0;
+    int right = strlen(str)-1; 
+
+    while(left<right){
+        char temp = str[left]; 
+        str[left] = str[right]; 
+        str[right] = temp; 
+        left++; 
+        right--; 
+    }
+
+    printf("%s\n", str); 
+
+
+}
