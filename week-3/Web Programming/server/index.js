af11cc18573b703cdd4c4e3b79deceaf0481cd58
bhuvitw <bhuvnesh6115@gmail.com>
2026-07-27 11:16:06 +0530

    updated server/index.js code

--- week-3/Web Programming/server/index.js
+++ week-3/Web Programming/server/index.js
@@ -1,35 +1,45 @@
 import * as http from "http"
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
 
-    res.end(JSON.stringify([
-        {
-            "id": 1,
-            "name": "Tom",
-            "isActive": true,
-        },
-        {
-            "id": 2,
-            "name": "John",
-            "isActive": false
-        },
-        {
-            "id": 3,
-            "name": "Jerry",
-            "isActive": true
-        },
-        {
-            "id": 4,
-            "name": "Alice",
-            "isActive": false
-        },
-        {
-            "id": 5,
-            "name": "Bob",
-            "isActive": true
-        }]
-    ))
+    if(req.url=== "/users" && req.method === "GET"){
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+        
+            res.end(JSON.stringify([
+                {
+                    "id": 1,
+                    "name": "Tom",
+                    "isActive": true,
+                },
+                {
+                    "id": 2,
+                    "name": "John",
+                    "isActive": false
+                },
+                {
+                    "id": 3,
+                    "name": "Jerry",
+                    "isActive": true
+                },
+                {
+                    "id": 4,
+                    "name": "Alice",
+                    "isActive": false
+                },
+                {
+                    "id": 5,
+                    "name": "Bob",
+                    "isActive": true
+                }]
+            ))
+    }else {
+        res.writeHead(200, { 'Content-Type' : 'application/json'}); 
+    
+        res.end(JSON.stringify({
+            "data": "Hello World",
+        }));
+    }
 })
 
 
