f35a924a140c704a984f6d616665429cdd746294
bhuvitw <bhuvnesh6115@gmail.com>
2026-09-22 19:35:43 +0530

    week-10(in progress)[solved Intersection of two Sorted Linkedlist]

--- week-10/Programming/intersectionOfSortedLinkedList/Main.java
+++ week-10/Programming/intersectionOfSortedLinkedList/Main.java
@@ -1,28 +1,37 @@
 public class Main {
 
-    public static void printList(ListNode head) {
-        ListNode curr = head; 
+    public static void printList(Node head) {
+        Node curr = head; 
         while( curr != null) {
             System.out.print(curr.val + ((curr.next!=null) ? "-> " : "-> null\n"));
             curr = curr.next; 
         }
     }
 
-    public static void main(String[] args){
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+    public static void main(String[] args) {
+        // List 1: 1 -> 2 -> 3 -> 4 -> 6
+        Node head1 = new Node(1);
+        head1.next = new Node(2);
+        head1.next.next = new Node(3);
+        head1.next.next.next = new Node(4);
+        head1.next.next.next.next = new Node(6);
 
-        System.out.println("Original: ");
-        printList(head); 
+        // List 2: 2 -> 4 -> 6 -> 8
+        Node head2 = new Node(2);
+        head2.next = new Node(4);
+        head2.next.next = new Node(6);
+        head2.next.next.next = new Node(8);
 
-        ReverseLinkedlist solver = new ReverseLinkedlist(); 
-        
-        ListNode reversedHead = solver.reverseLinkedlist(head); 
+        System.out.print("List 1: ");
+        printList(head1);
 
-        System.out.println("Reveresed: "); 
-        printList(reversedHead); 
+        System.out.print("List 2: ");
+        printList(head2);
+
+        IntersectionLinkedlist solver = new IntersectionLinkedlist();
+        Node intersectionHead = solver.findIntersection(head1, head2);
+
+        System.out.print("Intersection: ");
+        printList(intersectionHead);
     }
 }
