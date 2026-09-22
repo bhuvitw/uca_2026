313c7dbe91ebc8d5c3091a1f260acba7714b77cc
bhuvitw <bhuvnesh6115@gmail.com>
2026-09-22 19:45:32 +0530

    week-10(completed)[solved Search a linked list in another list]

--- week-10/Programming/searchInLinkedlist/Main.java
+++ week-10/Programming/searchInLinkedlist/Main.java
@@ -1,28 +1,58 @@
 public class Main {
 
-    public static void printList(ListNode head) {
-        ListNode curr = head; 
+    public static void printList(Node head) {
+        Node curr = head; 
         while( curr != null) {
             System.out.print(curr.val + ((curr.next!=null) ? "-> " : "-> null\n"));
             curr = curr.next; 
         }
     }
 
-    public static void main(String[] args){
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+    public static void main(String[] args) {
+        SearchLinkedlist solver = new SearchLinkedlist();
 
-        System.out.println("Original: ");
-        printList(head); 
+        // ----------------- Example 1 -----------------
+        // list1 = 10 -> 20
+        Node l1 = new Node(10);
+        l1.next = new Node(20);
 
-        ReverseLinkedlist solver = new ReverseLinkedlist(); 
-        
-        ListNode reversedHead = solver.reverseLinkedlist(head); 
+        // list2 = 5 -> 10 -> 20
+        Node l2 = new Node(5);
+        l2.next = new Node(10);
+        l2.next.next = new Node(20);
 
-        System.out.println("Reveresed: "); 
-        printList(reversedHead); 
+        System.out.println("--- Example 1 ---");
+        System.out.print("List 1: ");
+        printList(l1);
+        System.out.print("List 2: ");
+        printList(l2);
+
+        boolean res1 = solver.isSubList(l1, l2);
+        System.out.println("Output: " + (res1 ? "Yes" : "No")); // Expected: Yes
+
+        // ----------------- Example 3 -----------------
+        // list1 = 1 -> 2 -> 3 -> 4
+        Node l3 = new Node(1);
+        l3.next = new Node(2);
+        l3.next.next = new Node(3);
+        l3.next.next.next = new Node(4);
+
+        // list2 = 1 -> 2 -> 2 -> 1 -> 2 -> 3
+        Node l4 = new Node(1);
+        l4.next = new Node(2);
+        l4.next.next = new Node(2);
+        l4.next.next.next = new Node(1);
+        l4.next.next.next.next = new Node(2);
+        l4.next.next.next.next.next = new Node(3);
+
+        System.out.println("\n--- Example 3 ---");
+        System.out.print("List 1: ");
+        printList(l3);
+        System.out.print("List 2: ");
+        printList(l4);
+
+        boolean res2 = solver.isSubList(l3, l4);
+        System.out.println("Output: " + (res2 ? "Yes" : "No")); // Expected: No
     }
+
 }
